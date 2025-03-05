e67c631cb7db87c5e233668834334a745db0a69b
deva <vasudeva2002y@gmail.com>
2025-03-05 10:45:34 +0530

    intialized the course

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Transformations from "./Properties/Transformations";
+import Transitions from "./Properties/Transitions";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Transformations /> */}
+      <Transitions />
+    </div>
+  );
+};
+export default App;
