2eddff28e76a0e6a789abc8677576abddcb80b9b
deva <vasudeva2002y@gmail.com>
2025-03-05 18:58:35 +0530

    Completed Framer Motion

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+"use strict";
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+var vite_1 = require("vite");
+var plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/framer-motion-simplified/", // Ensure the correct repo name
+exports.default = (0, vite_1.defineConfig)({
+  plugins: [(0, plugin_react_1.default)()],
 });
